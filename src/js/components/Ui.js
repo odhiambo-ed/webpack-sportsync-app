e8b1ade35aea00bc473976bdb5a51b1e8a18b940
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-20 05:11:29 +0300

    remove lint

--- src/js/components/Ui.js
+++ src/js/components/Ui.js
@@ -1,16 +1,16 @@
 class Ui {
-    constructor() {
-        this.content = document.getElementById('content');
-    }
+  constructor() {
+    this.content = document.getElementById('content');
+  }
 
-    displaySports(sports) {
-        let output = '<h2>All Sports</h2><ul>';
-        sports.forEach(sport => {
-            output += `<li>${sport.strSport}</li>`;
-        });
-        output += '</ul>';
-        this.content.innerHTML = output;
-    }
+  displaySports(sports) {
+    let output = '<h2>All Sports</h2><ul>';
+    sports.forEach(sport => {
+      output += `<li>${sport.strSport}</li>`;
+    });
+    output += '</ul>';
+    this.content.innerHTML = output;
+  }
 }
 
 export default Ui;
