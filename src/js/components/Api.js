db564fbae3edad0c3e65563cc818829af47b9ff6
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-20 12:45:50 +0300

    update app js

--- src/js/components/Api.js
+++ src/js/components/Api.js
@@ -4,11 +4,17 @@ class SportsDataAPI {
   constructor() {
     this.baseURL = 'https://www.thesportsdb.com/api/v1/json/3/';
   }
+
   async getAllLeagues() {
     const response = await axios.get(`${this.baseURL}all_leagues.php`);
     return response.data.leagues;
   }
 
+  async fetchTeams(page) {
+    const response = await axios.get(`${this.baseURL}search_all_teams.php?l=English%20Premier%20League&page=${page}`);
+    return response.data.teams;
+  
+  }
 
   async searchTeams(teamName) {
     const response = await axios.get(`${this.baseURL}searchteams.php?t=${teamName}`);
