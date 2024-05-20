a04b7fc2458b0546e1f02b38211ed136b95d708c
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-20 22:01:05 +0300

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import App from './js/app';
 import './css/global.css';
 import './css/styles.css';
 
+
 // Wait for the DOM to be fully loaded before initializing the app
 document.addEventListener('DOMContentLoaded', () => {
   // Create a new instance of the App class
