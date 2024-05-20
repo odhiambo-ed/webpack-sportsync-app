750cfceb83b5ccbd6dd4066c72d0f9d0e3cbf675
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-20 09:44:00 +0300

    update api methods

--- src/js/components/Api.js
+++ src/js/components/Api.js
@@ -2,12 +2,11 @@ import axios from 'axios';
 
 class SportsDataAPI {
   constructor() {
-    this.apiKey = '3';
     this.baseURL = 'https://www.thesportsdb.com/api/v1/json/3/';
   }
-  async getAllSports() {
-    const response = await axios.get(`${this.baseURL}all_sports.php`);
-    return response.data.sports;
+  async getAllLeagues() {
+    const response = await axios.get(`${this.baseURL}all_leagues.php`);
+    return response.data.leagues;
   }
 
 
