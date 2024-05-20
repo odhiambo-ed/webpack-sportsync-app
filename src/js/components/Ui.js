ef9b8709d8027f580d0fd1856cda0ef3451bae6a
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-20 05:10:56 +0300

    add display sports function

--- src/js/components/Ui.js
+++ src/js/components/Ui.js
@@ -3,7 +3,14 @@ class Ui {
         this.content = document.getElementById('content');
     }
 
-   
+    displaySports(sports) {
+        let output = '<h2>All Sports</h2><ul>';
+        sports.forEach(sport => {
+            output += `<li>${sport.strSport}</li>`;
+        });
+        output += '</ul>';
+        this.content.innerHTML = output;
+    }
 }
 
 export default Ui;
