206b700c3f2f8e4382e8d60e78bdcf77cf110f3c
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-20 05:07:00 +0300

    fix lints

--- src/js/components/Api.js
+++ src/js/components/Api.js
@@ -1,20 +1,20 @@
 import axios from 'axios';
 
 class SportsDataAPI {
-    constructor() {
-        this.apiKey = '3';
-        this.baseURL = 'https://www.thesportsdb.com/api/v1/json/3/';
-    }
-    async getAllSports() {
-        const response = await axios.get(`${this.baseURL}all_sports.php`);
-        return response.data.sports;
-    }
+  constructor() {
+    this.apiKey = '3';
+    this.baseURL = 'https://www.thesportsdb.com/api/v1/json/3/';
+  }
+  async getAllSports() {
+    const response = await axios.get(`${this.baseURL}all_sports.php`);
+    return response.data.sports;
+  }
 
 
-    async searchTeams(teamName) {
-        const response = await axios.get(`${this.baseURL}searchteams.php?t=${teamName}`);
-        return response.data.teams;
-    }
+  async searchTeams(teamName) {
+    const response = await axios.get(`${this.baseURL}searchteams.php?t=${teamName}`);
+    return response.data.teams;
+  }
 }
 
 export default SportsDataAPI;
