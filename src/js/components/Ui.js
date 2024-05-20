c1838ffc0d5106deb5ea6251b85f6bc9b4ab7499
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-20 10:45:41 +0300

    add load more function

--- src/js/components/Ui.js
+++ src/js/components/Ui.js
@@ -1,12 +1,26 @@
 import '../../css/ui.css';
+
 class Ui {
   constructor() {
     this.content = document.getElementById('main');
+    this.leagues = [];
+    this.itemsPerPage = 5;
+    this.currentPage = 1;
   }
 
   displaySports(leagues) {
+    this.leagues = leagues;
+    this.currentPage = 1;
+    this.renderLeagues();
+  }
+
+  renderLeagues() {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = this.currentPage * this.itemsPerPage;
+    const paginatedLeagues = this.leagues.slice(start, end);
+
     let output = '<h2>All Leagues</h2><ul>';
-    leagues.forEach(league => {
+    paginatedLeagues.forEach(league => {
       output += `
         <li>
           <h3>${league.strLeague}</h3>
@@ -15,9 +29,27 @@ class Ui {
         </li>`;
     });
     output += '</ul>';
+
+    if (end < this.leagues.length) {
+      output += '<button id="loadMore">Load More</button>';
+    }
+
     this.content.innerHTML = output;
+    this.addLoadMoreEvent();
   }
-    
+
+  addLoadMoreEvent() {
+    const loadMoreBtn = document.getElementById('loadMore');
+    if (loadMoreBtn) {
+      loadMoreBtn.addEventListener('click', () => this.loadMoreLeagues());
+    }
+  }
+
+  loadMoreLeagues() {
+    this.currentPage++;
+    this.renderLeagues();
+  }
+
   showError(message) {
     this.content.innerHTML = `<div class="error">${message}</div>`;
   }
