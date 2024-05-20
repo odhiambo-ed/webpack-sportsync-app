d1ed4c4363b3c88cb9302fd320b5bd2052556156
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-20 06:28:44 +0300

    edit app js

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,23 @@
+// Import necessary modules
 import SportsDataAPI from './components/Api';
 import Ui from './components/Ui';
 import '../css/styles.css';
-
 class App {
   constructor() {
     this.api = new SportsDataAPI();
     this.ui = new Ui();
   }
 
+  // Initialize the app
   async init() {
-    const sports = await this.api.getAllSports();
-    this.ui.showSports(sports);
+    try {
+      const sports = await this.api.getAllSports();
+      this.ui.showSports(sports);
+    } catch (error) {
+      console.error('Error fetching sports data:', error);
+      this.ui.showError('Failed to load sports data. Please try again later.');
+    }
   }
 }
 
-const app = new App();
-app.init();
+export default App;
