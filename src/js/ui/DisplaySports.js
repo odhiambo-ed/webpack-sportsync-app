a9387890ffd0679dc54beed240c499013a924835
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-21 12:21:05 +0300

    edit lint

--- src/js/ui/DisplaySports.js
+++ src/js/ui/DisplaySports.js
@@ -1,52 +1,52 @@
 class DisplaySports {
-    constructor() {
-        this.content = document.getElementById('main');
-        this.leagues = [];
-        this.itemsPerPage = 5;
-        this.currentPage = 1;
-    }
+  constructor() {
+    this.content = document.getElementById('main');
+    this.leagues = [];
+    this.itemsPerPage = 5;
+    this.currentPage = 1;
+  }
 
-    display(leagues) {
-        this.leagues = leagues;
-        this.currentPage = 1;
-        this.renderLeagues();
-    }
+  display(leagues) {
+    this.leagues = leagues;
+    this.currentPage = 1;
+    this.renderLeagues();
+  }
 
-    renderLeagues() {
-        const start = (this.currentPage - 1) * this.itemsPerPage;
-        const end = this.currentPage * this.itemsPerPage;
-        const paginatedLeagues = this.leagues.slice(start, end);
+  renderLeagues() {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = this.currentPage * this.itemsPerPage;
+    const paginatedLeagues = this.leagues.slice(start, end);
 
-        let output = '<h2>All Leagues</h2><ul>';
-        paginatedLeagues.forEach(league => {
-            output += `
+    let output = '<h2>All Leagues</h2><ul>';
+    paginatedLeagues.forEach(league => {
+      output += `
         <li>
           <h3>${league.strLeague}</h3>
           <p>Sport: ${league.strSport}</p>
           <p>Alternate Names: ${league.strLeagueAlternate || 'N/A'}</p>
         </li>`;
-        });
-        output += '</ul>';
+    });
+    output += '</ul>';
 
-        if (end < this.leagues.length) {
-            output += '<button id="loadMoreLeagues">Load More Leagues</button>';
-        }
-
-        this.content.innerHTML = output;
-        this.addLoadMoreEvent();
+    if (end < this.leagues.length) {
+      output += '<button id="loadMoreLeagues">Load More Leagues</button>';
     }
 
-    addLoadMoreEvent() {
-        const loadMoreBtn = document.getElementById('loadMoreLeagues');
-        if (loadMoreBtn) {
-            loadMoreBtn.addEventListener('click', () => this.loadMoreLeagues());
-        }
-    }
+    this.content.innerHTML = output;
+    this.addLoadMoreEvent();
+  }
 
-    loadMoreLeagues() {
-        this.currentPage++;
-        this.renderLeagues();
+  addLoadMoreEvent() {
+    const loadMoreBtn = document.getElementById('loadMoreLeagues');
+    if (loadMoreBtn) {
+      loadMoreBtn.addEventListener('click', () => this.loadMoreLeagues());
     }
+  }
+
+  loadMoreLeagues() {
+    this.currentPage++;
+    this.renderLeagues();
+  }
 }
 
 export default DisplaySports;
