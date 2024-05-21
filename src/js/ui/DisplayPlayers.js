5ce35d74908c98ca6796f759e4195f91c1d9d194
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-21 12:32:38 +0300

    add display players function

---
+++ src/js/ui/DisplayPlayers.js
@@ -0,0 +1,23 @@
+class DisplayPlayers {
+    constructor() {
+        this.playersContainer = document.getElementById('playersContainer');
+    }
+
+    display(players) {
+        if (!this.playersContainer) return;
+
+        let output = '<h2>Players</h2><ul>';
+        players.forEach(player => {
+            output += `
+        <li>
+          <h3>${player.strPlayer}</h3>
+          <p>Position: ${player.strPosition}</p>
+          <p>Nationality: ${player.strNationality}</p>
+        </li>`;
+        });
+        output += '</ul>';
+        this.playersContainer.innerHTML = output;
+    }
+}
+
+export default DisplayPlayers;
