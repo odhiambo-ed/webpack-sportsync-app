9b362edc5d0c5a83a845f96ba1dd529f04b4efe4
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-22 18:48:53 +0300

    update display event function

--- src/js/ui/DisplayEvents.js
+++ src/js/ui/DisplayEvents.js
@@ -6,23 +6,24 @@ class DisplayEvents {
   display(eventHighlights) {
     if (!this.content) return;
 
-    let output = '<div class="card" style="width: 18rem;">';
+    let output = '';
     eventHighlights.forEach(event => {
-      output += `
+      output += `<div class="card" style="width: 90%">
         <div class="card-header">
-    ${event.strEvent}
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="d-flex flex-column list-group-item">
-          <div class="d-flex flex-row gap-1 align-items-center justify-content-center">
-            <p>${event.dateEvent}</p>
-            <img src="${event.strThumb}" alt="${event.strEvent}">
-          </div>
-          <p><a href="${event.strVideo}" target="_blank">Watch Highlights</a></p>
-    </li>
-  </ul>`;
+          ${event.strEvent}
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="d-flex flex-column list-group-item">
+            <div class="d-flex flex-row gap-1 align-items-center justify-content-center">
+              <p>${event.dateEvent}</p>
+              <img src="${event.strThumb}" alt="${event.strEvent}" style="max-width: 100px; max-height: 100px;">
+            </div>
+            <p><a href="${event.strVideo}" target="_blank">Watch Highlights</a></p>
+          </li>
+        </ul>
+      </div>`;
     });
-    output += '</div>';
+
     this.content.innerHTML = output;
   }
 }
