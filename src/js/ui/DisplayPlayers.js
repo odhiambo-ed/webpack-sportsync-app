326dd5a9bb5d8b9f392984d8aef6bbe3e00e5bee
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-22 06:22:28 +0300

    add display players function

--- src/js/ui/DisplayPlayers.js
+++ src/js/ui/DisplayPlayers.js
@@ -18,6 +18,24 @@ class DisplayPlayers {
     output += '</ul>';
     this.playersContainer.innerHTML = output;
   }
+
+  displayMilestones(milestones) {
+    if (!this.playersContainer) return;
+
+    let output = '<h2>Player Milestones</h2><ul>';
+    milestones.forEach(milestone => {
+      output += `
+        <li>
+          <h3>${milestone.strMilestone}</h3>
+          <p>Player: ${milestone.strPlayer}</p>
+          <p>Team: ${milestone.strTeam}</p>
+          <p>Date: ${milestone.dateMilestone}</p>
+          <img src="${milestone.strMilestoneLogo}" alt="${milestone.strMilestone}">
+        </li>`;
+    });
+    output += '</ul>';
+    this.playersContainer.innerHTML = output;
+  }
 }
 
 export default DisplayPlayers;
