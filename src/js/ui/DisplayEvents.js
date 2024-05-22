fe43a652e2de714d5d0cede6a070baa77a06157f
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-22 14:24:03 +0300

    add card

--- src/js/ui/DisplayEvents.js
+++ src/js/ui/DisplayEvents.js
@@ -6,17 +6,23 @@ class DisplayEvents {
   display(eventHighlights) {
     if (!this.content) return;
 
-    let output = '<ul>';
+    let output = '<div class="card" style="width: 18rem;">';
     eventHighlights.forEach(event => {
       output += `
-        <li>
-          <h3>${event.strEvent}</h3>
-          <p>${event.dateEvent}</p>
-          <img src="${event.strThumb}" alt="${event.strEvent}">
+        <div class="card-header">
+    ${event.strEvent}
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="d-flex flex-column list-group-item">
+          <div class="d-flex flex-row gap-1 align-items-center justify-content-center">
+            <p>${event.dateEvent}</p>
+            <img src="${event.strThumb}" alt="${event.strEvent}">
+          </div>
           <p><a href="${event.strVideo}" target="_blank">Watch Highlights</a></p>
-        </li>`;
+    </li>
+  </ul>`;
     });
-    output += '</ul>';
+    output += '</div>';
     this.content.innerHTML = output;
   }
 }
