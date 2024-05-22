94e86d050f5f230742a0a0e146695a4dd5e13f19
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-22 21:19:51 +0300

    add load more

--- src/js/ui/DisplayTeams.js
+++ src/js/ui/DisplayTeams.js
@@ -1,12 +1,24 @@
 class DisplayTeams {
   constructor() {
     this.teamsContainer = document.getElementById('teamsContainer');
+    this.teamsPerPage = 6;
+    this.currentPage = 0;
+    this.teams = [];
   }
 
   display(teams) {
     if (!this.teamsContainer) return;
 
-    teams.forEach(team => {
+    this.teams = teams;
+    this.renderPage();
+  }
+
+  renderPage() {
+    const start = this.currentPage * this.teamsPerPage;
+    const end = start + this.teamsPerPage;
+    const teamsToShow = this.teams.slice(start, end);
+
+    teamsToShow.forEach(team => {
       const teamCard = document.createElement('div');
       teamCard.className = 'col-md-2 team-card animate__animated animate__fadeInUp';
       teamCard.innerHTML = `
@@ -19,6 +31,11 @@ class DisplayTeams {
         </div>`;
       this.teamsContainer.appendChild(teamCard);
     });
+  }
+
+  loadMore() {
+    this.currentPage++;
+    this.renderPage();
   }
 }
 
