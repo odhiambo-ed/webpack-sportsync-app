4d3d2949155bf7a0a48c660dab2f55ef267c6ecb
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-22 20:43:41 +0300

    style display leagues

--- src/js/ui/DisplaySports.js
+++ src/js/ui/DisplaySports.js
@@ -2,7 +2,7 @@ class DisplaySports {
   constructor() {
     this.content = document.getElementById('leaguesContainer');
     this.leagues = [];
-    this.itemsPerPage = 5;
+    this.itemsPerPage = 3;
     this.currentPage = 1;
   }
 
@@ -17,20 +17,20 @@ class DisplaySports {
     const end = this.currentPage * this.itemsPerPage;
     const paginatedLeagues = this.leagues.slice(start, end);
 
-    let output = '<h2 class="animate__animated animate__fadeInUp">All Leagues</h2><ul>';
+    let output = '<h2 class="animate__animated animate__fadeInUp text-uppercase">Top Leagues</h2><hr><ul class="d-flex flex-row">';
     paginatedLeagues.forEach(league => {
       output += `
         <li>
-          <h3>${league.strLeague}</h3>
+          <h5 class="text-uppercase">${league.strLeague}</h5>
           <p>Sport: ${league.strSport}</p>
           <p>Alternate Names: ${league.strLeagueAlternate || 'N/A'}</p>
-          <img src="${league.strBadge}" alt="${league.strLeague}" />
+          <img style="height: 100px" src="${league.strBadge}" alt="${league.strLeague}" />
         </li>`;
     });
     output += '</ul>';
 
     if (end < this.leagues.length) {
-      output += '<button id="loadMoreLeagues">Load More Leagues</button>';
+      output += '<div class="row"><button id="loadMoreLeagues" class="btn btn-primary mt-4 animate__animated animate__fadeInUp w-50 position-absolute start-50 translate-middle"> Load More Leagues</button></div>';
     }
 
     this.content.innerHTML = output;
