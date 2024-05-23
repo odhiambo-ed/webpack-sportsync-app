a4c2100766e18a0276838836957c0477c69f95e2
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-23 19:02:31 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,75 +2,67 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',  // The output filename for the bundled JavaScript
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',  // The public URL of the output directory when referenced in a browser
+    publicPath: '/',
   },
-  devtool: 'eval-source-map',  // Source map mode for development
+  devtool: 'eval-source-map',
   plugins: [
-    new ESLintPlugin(),  // Plugin for linting JavaScript
-    new CleanWebpackPlugin({ verbose: true }),  // Clean the output directory before emit
+    new ESLintPlugin(),
+    new CleanWebpackPlugin({ verbose: true }),
     new HtmlWebpackPlugin({
-      title: 'Sport Sync App',  // The title of the generated HTML page
-      template: './src/index.html',  // Template file for the HTML
-      inject: 'body'  // Inject all assets into the body element
-    })
+      title: 'Sport Sync App',
+      template: './src/index.html',
+      inject: 'body',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.(scss)$/,
         use: [
-          {
-            // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: 'style-loader'
-          },
-          {
-            // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: 'css-loader'
-          },
-          {
-            // Loader for webpack to process CSS with PostCSS
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [
-                  require('autoprefixer')
-                ]
-              }
-            }
-          },
-          {
-            // Loads a SASS/SCSS file and compiles it to CSS
-            loader: 'sass-loader'
-          }
-        ]
+          MiniCssExtractPlugin.loader, // Extracts CSS into separate files
+          'css-loader', // Turns CSS into CommonJS
+          'postcss-loader', // Loader for webpack to process CSS with PostCSS
+          'sass-loader', // Compiles Sass to CSS
+        ],
       },
       {
-        test: /\.(gif|png|avif|jpe?g)$/,  // Test for image files
-        type: 'asset/resource',  // The type of module created
+        test: /\.(css)$/,
+        use: [
+          MiniCssExtractPlugin.loader, // Extracts CSS into separate files
+          'css-loader', // Turns CSS into CommonJS
+        ],
+      },
+      {
+        test: /\.(gif|png|avif|jpe?g)$/,
+        type: 'asset/resource',
         generator: {
-          filename: 'assets/images/[name][ext]',  // Output path and filename for images
+          filename: 'assets/images/[name][ext]',
         },
       },
       {
-        test: /\.html$/,  // Test for HTML files
+        test: /\.html$/,
         use: [
-          'html-loader'  // Exports HTML as a string
-        ]
+          'html-loader',
+        ],
       },
-    ]
+    ],
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),  // Directory for static files
+      directory: path.join(__dirname, 'dist'),
     },
-    port: 9000,  // Port for the development server
-    historyApiFallback: true,  // Enable support for history API fallback
-    open: true,  // Open the browser after server starts
+    port: 9000,
+    historyApiFallback: true,
+    open: true,
   },
 };
