4a94a5756d8e67c9bd1f49159cde2e546456c7ed
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-23 21:02:14 +0300

    remove bootstrap npm and add cdn

--- webpack.config.js
+++ webpack.config.js
@@ -23,31 +23,10 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(scss)$/,
+        test: /\.css$/,
         use: [
-          {
-            // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: 'style-loader'
-          },
-          {
-            // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: 'css-loader'
-          },
-          {
-            // Loader for webpack to process CSS with PostCSS
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [
-                  require('autoprefixer')
-                ]
-              }
-            }
-          },
-          {
-            // Loads a SASS/SCSS file and compiles it to CSS
-            loader: 'sass-loader'
-          }
+          'style-loader',
+          'css-loader'
         ]
       },
       {
